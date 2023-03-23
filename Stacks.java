57ddf05552bbf8fbbf41cdf0fbfc113d100017b8
drisyaathul <drisyak809@gmail.com>
2023-03-24 01:35:52 +0800

    Added: Peak and Pop from the Stack till it is Empty

--- Stacks.java
+++ Stacks.java
@@ -6,11 +6,18 @@ public class Stacks<T> {
     LinkedList <T> linkedList = new LinkedList<>();
 
     public void push(T data) {
-
         linkedList.push(data);
     }
     public void display() {
-
         linkedList.display();
+    }
+    public T pop() {
+        return linkedList.pop();
+    }
+    public void peek() {
+
+        Integer peekedData = (Integer) linkedList.pop();
+        System.out.println("The Peek Element = "+peekedData);
+        linkedList.push((T) peekedData);
     }
 }
